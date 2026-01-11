5d8707514628e0d801ad6ba5b4c81ac01f690659
mayonorris <kadanganorris@gmail.com>
2026-01-11 22:25:01 +0000

    user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <UserProfile
         name="Alice"
         age="25"
-        bio="Loves hiking and photography"
+        Bio="Loves hiking and photography"
       />
     </>
   );
