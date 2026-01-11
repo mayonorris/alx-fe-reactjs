93109a28380a93681d119c65fd3dc295be5dfc97
mayonorris <kadanganorris@gmail.com>
2026-01-11 22:23:29 +0000

    user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,15 @@
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import UserProfile from './UserProfile';
 
 function App() {
   return (
     <>
-      <Header />
-      <MainContent />
-      <Footer />
+      {/* Other components can stay above/below */}
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
     </>
   );
 }
-
 export default App;
