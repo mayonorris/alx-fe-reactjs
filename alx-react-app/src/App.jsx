3c356264c37047e2c35fa6f599fdc1fa1640a982
mayonorris <kadanganorris@gmail.com>
2026-01-11 22:17:11 +0000

    Update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,15 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
     <>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
+
 export default App;
