93b954dc0ca1b0f27ec82f9049ddb7efa91dc575
mayonorris <kadanganorris@gmail.com>
2026-02-01 15:41:02 +0000

    Task 0: Set up React project with Vite, and  Axios

---
+++ github-user-search/github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GitHub User Search</h1>
+      {/* Search component will go here */}
+    </div>
+  );
+}
+
+export default App;
